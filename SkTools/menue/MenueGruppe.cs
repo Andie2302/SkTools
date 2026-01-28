b072dceab92da119a95004a9f23707332362c12c
andreas <Andreas.Zwander@gmail.com>
2026-01-28 04:38:45 +0100

    Update `MenueGruppe` property comments for clarity and data annotation improvements

--- SkTools/menue/MenueGruppe.cs
+++ SkTools/menue/MenueGruppe.cs
@@ -1,9 +1,9 @@
 namespace SkTools.menue;
 
 public record MenueGruppe(
-    string Name,  //Name der Gruppe
-    int Id, //Id der Gruppe (Ganzzahl)
-    string Color, // Farbe der Gruppe (HEX-Code)
-    bool Einblenden, // Wird die Gruppe angezeigt? (true/false)
-    List<MenueEintrag> MenueEinträge // Liste der Menüeinträge in dieser Gruppe
+    int Id, // Primärschlüssel
+    string Name,
+    string Color,
+    bool Einblenden,
+    List<MenueEintrag> MenueEinträge // EF Core erkennt dies als 1-zu-n Beziehung
 );
