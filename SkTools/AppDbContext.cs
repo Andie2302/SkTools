e3d0ea6b4a86efc0bec40cedaca70d80732d3b88
andreas <Andreas.Zwander@gmail.com>
2026-01-28 04:38:36 +0100

    Add `MenueGruppe` and `MenueEintrag` DbSet properties to `AppDbContext`

--- SkTools/AppDbContext.cs
+++ SkTools/AppDbContext.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using SkTools.menue; // Namespace für die neuen Klassen
 
 namespace SkTools;
 
 public class AppDbContext : DbContext
 {
     public DbSet<RecordEntry> Records => Set<RecordEntry>();
+    
+    // Neue Tabellen für Menüs
+    public DbSet<MenueGruppe> MenueGruppen => Set<MenueGruppe>();
+    public DbSet<MenueEintrag> MenueEintraege => Set<MenueEintrag>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite("Data Source=meine_datenbank.db");
