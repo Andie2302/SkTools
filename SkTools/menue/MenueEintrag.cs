4c7e0c81d1bc24bd6861c0bfc3659ae662b749c1
andreas <Andreas.Zwander@gmail.com>
2026-01-28 04:38:40 +0100

    Add `MenueGruppeId` foreign key to `MenueEintrag` and update property comments

--- SkTools/menue/MenueEintrag.cs
+++ SkTools/menue/MenueEintrag.cs
@@ -1,11 +1,12 @@
 namespace SkTools.menue;
 
 public record MenueEintrag(
-    string NameLang, // Name des Eintrags
-    string NameKurz, // Kurzer Name des Eintrags
-    int Id, // Id des Eintrags (Ganzzahl)
-    string Color, // Farbe des Eintrags (HEX-Code)
-    bool IstTrenner, // Ist dieser Eintrag ein Trenner? (true/false)
-    decimal Price, // Preis des Eintrags (Dezimalzahl)
-    decimal Mehrwertsteuer // Steuerbetrag in Prozent
+    int Id, // Primärschlüssel
+    string NameLang,
+    string NameKurz,
+    string Color,
+    bool IstTrenner,
+    decimal Price,
+    decimal Mehrwertsteuer,
+    int MenueGruppeId // Fremdschlüssel zur Gruppe
 );
