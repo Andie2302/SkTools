9817a5547d20bf1b47b128c9013ddb2cb5f1f54a
andreas <Andreas.Zwander@gmail.com>
2026-01-28 04:02:29 +0100

    Add `MenueEintrag` record to represent menu entry model in SkTools

---
+++ SkTools/menue/MenueEintrag.cs
@@ -0,0 +1,21 @@
+namespace SkTools;
+
+public record MenueEintrag(
+    string NameLong,
+    string NameShort,
+    string Id,
+    string Color,
+    string EmptyItem,
+    string Price,
+    string Vat,
+    string ImmediateEdit,
+    string ImmediateField,
+    string AccountNumber,
+    string ArticleNumber,
+    string EanCode,
+    string PrinterId,
+    string ExtraIds,
+    string Favorite,
+    string FavoriteIndex,
+    string FavoriteColor
+);
