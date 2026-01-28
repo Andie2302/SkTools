90c1e0ae8e71d6156299028538c616b54bc5596c
andreas <Andreas.Zwander@gmail.com>
2026-01-28 04:12:16 +0100

    Refactor `MenueEintrag` properties to use accurate data types and add property comments for clarity

--- SkTools/menue/MenueEintrag.cs
+++ SkTools/menue/MenueEintrag.cs
@@ -1,21 +1,21 @@
 namespace SkTools.menue;
 
 public record MenueEintrag(
-    string NameLong,
-    string NameShort,
-    string Id,
-    string Color,
-    string EmptyItem,
-    string Price,
-    string Vat,
-    string ImmediateEdit,
-    string ImmediateField,
-    string AccountNumber,
-    string ArticleNumber,
-    string EanCode,
-    string PrinterId,
-    string ExtraIds,
-    string Favorite,
-    string FavoriteIndex,
-    string FavoriteColor
+    string NameLong, // Name des Eintrags
+    string NameShort, // Kurzer Name des Eintrags
+    int Id, // Id des Eintrags (Ganzzahl)
+    string Color, // Farbe des Eintrags (HEX-Code)
+    bool EmptyItem, // Ist das Eintrag Trennlinie? (true/false) false leerer string... true = 1
+    decimal Price, // Preis des Eintrags (Dezimalzahl)
+    int Vat, // Steuerbetrag in Prozent (Ganzzahl)
+    bool ImmediateEdit, // Unbekannte Eigenschaft
+    string ImmediateField, // Unbekannte Eigenschaft
+    string AccountNumber, // Konto-Nr.
+    string ArticleNumber, // Artikelnummer Intern
+    string EanCode, // EAN-Code (falls vorhanden)
+    int PrinterId, // Id des Druckers (Ganzzahl)
+    string ExtraIds, // Unbekannte Eigenschaft
+    string Favorite, // Unbekannte Eigenschaft
+    string FavoriteIndex, // Unbekannte Eigenschaft
+    string FavoriteColor // Unbekannte Eigenschaft
 );
